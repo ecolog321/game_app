dc7a23392dd79ca3f192ba1202644e8acdb2b7a5
Dmitriy Arkhipov <ecarkhipov@gmail.com>
2022-11-25 20:03:31 +0800

    4.3 complite

---
+++ src/cards.js
@@ -0,0 +1,184 @@
+/* eslint-disable prettier/prettier */
+// eslint-disable-next-line no-unused-vars
+let cards = [
+  {
+    number: 6,
+    suit: "peaks",
+    sourse: "../cards_images/6%20бубны.svg",
+  },
+  {
+    number: 7,
+    suit: "peaks",
+    sourse: "../cards_images/7%20пики.svg",
+  },
+  {
+    number: 8,
+    suit: "peaks",
+    sourse: "../cards_images/8%20пики.svg",
+  },
+  {
+    number: 9,
+    suit: "peaks",
+    sourse: "../cards_images/9%20пики.svg",
+  },
+  {
+    number: 10,
+    suit: "peaks",
+    sourse: "../cards_images/10%20пики.svg",
+  },
+  {
+    number: 11,
+    suit: "peaks",
+    sourse: "../cards_images/валет%20пики.svg",
+  },
+  {
+    number: 12,
+    suit: "peaks",
+    sourse: "../cards_images/дама%20пики.svg",
+  },
+  {
+    number: 13,
+    suit: "peaks",
+    sourse: "../cards_images/король%20пики.svg",
+  },
+  {
+    number: 14,
+    suit: "peaks",
+    sourse: "../cards_images/туз%20пики.svg",
+  },
+  {
+    number: 6,
+    suit: "hearts",
+    sourse: "../cards_images/6%20черви.svg",
+  },
+  {
+    number: 7,
+    suit: "hearts",
+    sourse: "../cards_images/7%20черви.svg",
+  },
+  {
+    number: 8,
+    suit: "hearts",
+    sourse: "../cards_images/8%20черви.svg",
+  },
+  {
+    number: 9,
+    suit: "hearts",
+    sourse: "../cards_images/9%20черви.svg",
+  },
+  {
+    number: 10,
+    suit: "hearts",
+    sourse: "../cards_images/10%20черви.svg",
+  },
+  {
+    number: 11,
+    suit: "hearts",
+    sourse: "../cards_images/валет%20черви.svg",
+  },
+  {
+    number: 12,
+    suit: "hearts",
+    sourse: "../cards_images/дама%20черви.svg",
+  },
+  {
+    number: 13,
+    suit: "hearts",
+    sourse: "../cards_images/король%20черви.svg",
+  },
+  {
+    number: 14,
+    suit: "hearts",
+    sourse: "../cards_images/туз%20черви.svg",
+  },
+  {
+    number: 6,
+    suit: "baptize",
+    sourse: "../cards_images/6%20крести.svg",
+  },
+  {
+    number: 7,
+    suit: "baptize",
+    sourse: "../cards_images/7%20крести.svg",
+  },
+  {
+    number: 8,
+    suit: "baptize",
+    sourse: "../cards_images/8%20крести.svg",
+  },
+  {
+    number: 9,
+    suit: "baptize",
+    sourse: "../cards_images/9%20крести.svg",
+  },
+  {
+    number: 10,
+    suit: "baptize",
+    sourse: "../cards_images/10%20крести.svg",
+  },
+  {
+    number: 11,
+    suit: "baptize",
+    sourse: "../cards_images/валет%20крести.svg",
+  },
+  {
+    number: 12,
+    suit: "baptize",
+    sourse: "../cards_images/дама%20крести.svg",
+  },
+  {
+    number: 13,
+    suit: "baptize",
+    sourse: "../cards_images/король%20крести.svg",
+  },
+  {
+    number: 14,
+    suit: "baptize",
+    sourse: "../cards_images/туз%20крести.svg",
+  },
+  {
+    number: 6,
+    suit: "diamonds",
+    sourse: "../cards_images/6%20бубны.svg",
+  },
+  {
+    number: 7,
+    suit: "diamonds",
+    sourse: "../cards_images/7%20бубны.svg",
+  },
+  {
+    number: 8,
+    suit: "diamonds",
+    sourse: "../cards_images/8%20бубны.svg",
+  },
+  {
+    number: 9,
+    suit: "diamonds",
+    sourse: "../cards_images/9%20бубны.svg",
+  },
+  {
+    number: 10,
+    suit: "diamonds",
+    sourse: "../cards_images/10%20бубны.svg",
+  },
+  {
+    number: 11,
+    suit: "diamonds",
+    sourse: "../cards_images/валет%20бубны.svg",
+  },
+  {
+    number: 12,
+    suit: "diamonds",
+    sourse: "../cards_images/дама%20бубны.svg",
+  },
+  {
+    number: 13,
+    suit: "diamonds",
+    sourse: "../cards_images/король%20бубны.svg",
+  },
+  {
+    number: 14,
+    suit: "diamonds",
+    sourse: "../cards_images/туз%20бубны.svg",
+  },
+]
